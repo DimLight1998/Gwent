a29a50353cf323a3a58036800dbe5d6a57520db5
Zhang Yang <dimlight1998@gmail.com>
2017-09-03 14:04:56 +0800

    Initial commit.

---
+++ Main.cpp
@@ -0,0 +1,11 @@
+//
+// Created on 2017/09/03 at 14:01.
+//
+#include <QApplication>
+
+
+int main(int argc, char **argv)
+{
+    QApplication application(argc, argv);
+    return application.exec();
+}
